3c7b8dcef7e28a85289b42d55abe140dce1ec282
Gabriel <gabriel.funck45@gmail.com>
2024-07-01 02:26:42 -0300

    Criação da classe que contém a coloração para as Esferas aparecerem no terminal
    Co-authored-by: LuisaWentz <LuisaWentz@users.noreply.github.com>

---
+++ Terminal.java
@@ -0,0 +1,11 @@
+public class Terminal {
+    //CÓDIGOS DAS CORES PARA AS ESFERAS/ELEMENTOS DO TABULEIRO
+    public static final String RESET = "\u001b[0m";              //PADRÃO
+    public static final String CAVEIRA = "\u001b[38;5;243m";     //CINZA
+    public static final String ROXO = "\u001b[38;5;055m";
+    public static final String BORDO = "\u001b[38;5;088m";
+    public static final String AZUL = "\u001b[38;5;081m";
+    public static final String VERDE = "\u001b[38;5;046m";
+    public static final String OURO = "\u001b[38;5;227m";        //AMARELO
+    public static final String EXP = "\u001b[38;5;231m";         //BRANCA
+}
