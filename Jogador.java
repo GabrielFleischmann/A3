d83913b4cb2ca4a33fae0fe3d0c93e6985a302b1
Gabriel <gabriel.funck45@gmail.com>
2024-07-01 02:08:18 -0300

    Criando classe Jogador
    Co-authored-by: ElisaAAgostini <ElisaAAgostini@users.noreply.github.com>
    Co-authored-by: lulizaoooooo@gmail.com <lulizaoooooo@gmail.com>
    Co-authored-by: LuisaWentz <LuisaWentz@users.noreply.github.com>

---
+++ Jogador.java
@@ -0,0 +1,75 @@
+public class Jogador {
+
+    private String nome;
+    private int vidaMax = 100;
+    private int vida = 100;
+    private int ouro = 0;
+    private int experiencia = 0;
+    private int turnoExtra = 0;
+    private int multiplicadorDeDanoDoAtaque = 1;
+
+    public Jogador (String nome){
+        this.nome = nome;
+    }
+
+    public int getMultiplicadorDeDanoDoAtaque(){
+        return multiplicadorDeDanoDoAtaque;
+    }
+
+    public void setMultiplicadorDeDanoDoAtaque(int multiplicadorDeDanoDoAtaque){
+        this.multiplicadorDeDanoDoAtaque = multiplicadorDeDanoDoAtaque;
+    }
+
+    public void setTurnoExtra(int numero){
+        this.turnoExtra = numero;
+    }
+
+    public int getTurnoExtra(){
+        return turnoExtra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVidaMax(){
+        return vidaMax;
+    }
+
+    public void setVidaMax(int vidaMax){
+        this.vidaMax = vidaMax;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getOuro() {
+        return ouro;
+    }
+
+    public void setOuro(int ouro) {
+        this.ouro = ouro;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    @Override
+    public String toString(){
+        return getNome() + "\nVida:" + getVida() + "/" + getVidaMax() + "\nOuro: " + getOuro() + "\nExp: " + getExperiencia();
+    }
+}
